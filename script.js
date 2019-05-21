6a1ad49f8dffe6f0fc44a1324f14be450aae6e3a
Daniel Corcoran <danielc.92@outlook.com>
2019-05-21 15:13:04 +1000

    Added index and script files

---
+++ script.js
@@ -0,0 +1,13 @@
+var myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
+    },,
+    options: options
+});
