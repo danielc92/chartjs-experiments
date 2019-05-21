325eb86447498ccb40da764a5774069ef4f2282b
Daniel Corcoran <danielc.92@outlook.com>
2019-05-21 16:44:24 +1000

    Dual chart tested

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 Chart.defaults.global.defaultFontFamily = 'monospace';
 
 var ctx = document.getElementById('myChart').getContext('2d');
+var ctx2 = document.getElementById('myChart2').getContext('2d');
 
 function done(){
   var url = chart.toBase64Image();
@@ -23,7 +24,8 @@ var options = {
     scales: {
     	yAxes: [{
     		ticks: {
-    			beginAtZero: true
+    			beginAtZero: true,
+
     		}
     	}]
     }
@@ -32,31 +34,86 @@ var options = {
 
 var chart = new Chart(ctx, {
     // The type of chart we want to create
-    type: 'bar',
+    type: 'scatter',
 
     // The data for our dataset
     data: {
-        labels: ['Apples', 'Bananas', 'Carrots', 'Kiwi', 'Watermelon'],
-        datasets: [{
-            label: 'Season 1 Harvest',
-            backgroundColor: 'rgba(0,0,255,.8)',
-            // borderColor: 'rgb(0,0,0)',
-            data: [30, -20, 5, 15, -10]
-        },
-        {
-            label: 'Season 2 Harvest',
-            backgroundColor: 'rgba(255,0,0,.8)',
-            // borderColor: 'rgb(0,0,0)',
-            data: [20, 10, 5, 15, 8]
-        },
-        {
-            label: 'Season 3 Harvest',
-            backgroundColor: 'rgba(0,233,0,.8)',
-            // borderColor: 'rgb(0,0,0)',
-            data: [8, 20, 12, 15,12]
+
+    	datasets: [{
+            label: 'Scatter Dataset',
+            pointRadius: 5,
+            backgroundColor: 'rgba(0,0,255,0.8)',
+            data: [{
+                x: -10,
+                y: 0
+            }, {
+                x: 0,
+                y: 10
+            }, {
+                x: 10,
+                y: 5
+            }, {
+                x: 0,
+                y: 10
+            }, {
+                x: 32,
+                y: 32
+            }, {
+                x: 12,
+                y: 32
+            }, {
+                x: 45,
+                y: 40
+            }]
         }]
+        // labels: ['Apples', 'Bananas', 'Carrots', 'Kiwi', 'Watermelon'],
+        // datasets: [{
+        //     label: 'Season 1 Harvest',
+        //     backgroundColor: 'rgba(0,0,255,0.7)',
+        //     data: [30, -20, 5, 15, -10]
+        // },
+        // {
+        //     label: 'Season 2 Harvest',
+        //     backgroundColor: 'rgba(255,0,0,.8)',
+        //     data: [20, 10, 5, 15, 8]
+        // },
+        // {
+        //     label: 'Season 3 Harvest',
+        //     backgroundColor: 'rgba(0,233,0,.8)',
+        //     data: [8, 20, 12, 15,12]
+        // }]
     },
 
     // Configuration options go here
     options: options
 });
+
+var chart2 = new Chart(ctx2, {
+    // The type of chart we want to create
+    type: 'bar',
+
+    // The data for our dataset
+    data: {
+    	labels: ['Watermelon', 'Kiwi', 'Potato', 'Carrot', 'Apple'],
+    	datasets: [{
+            label: 'Season 1 Harvest',
+            backgroundColor: 'rgba(0,0,255,0.7)',
+            data: [30, -20, 5, 15, -10]
+        },
+        {
+            label: 'Season 2 Harvest',
+            backgroundColor: 'rgba(255,0,0,.8)',
+            data: [20, 10, 5, 15, 8]
+        },
+        {
+            label: 'Season 3 Harvest',
+            backgroundColor: 'rgba(0,233,0,.8)',
+            data: [8, 20, 12, 15,12]
+        }
+    ]
+    },
+
+    // Configuration options go here
+    options: options
+});
+
